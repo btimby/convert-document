a51f44d0da4a73fb6e7cadf047ff52c40468b1ac
Ben Timby <btimby@smartfile.com>
2019-09-25 02:45:44 -0400

    Some safety.

--- preview/circusd.py
+++ preview/circusd.py
@@ -1,10 +1,12 @@
 from easy_circus.client import Client
 
 
-def send_command(cmd, watcher):
-    print('Sending %s to %s' % (cmd, watcher))
+def send_command(cmd_name, watcher):
+    print('Sending %s to %s' % (cmd_name, watcher))
     client = Client(host='127.0.0.1', port=5555, timeout=15)
-    cmd = getattr(client, cmd)
+    cmd = getattr(client, cmd_name)
+    if not callable(cmd):
+        raise ValueError('Invalid cmd_name %s' % cmd_name)
     cmd(watcher=watcher)
 
 
