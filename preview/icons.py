166c1acffda07656aea7f1a3bba53261dd395602
Ben Timby <btimby@gmail.com>
2020-07-03 18:42:14 -0400

    le sigh.

--- preview/icons.py
+++ preview/icons.py
@@ -50,7 +50,7 @@ def _get_best_fit(extension, width, height):
     if not isfile(pathjoin(ICON_ROOT, icon_path)):
         LOGGER.debug('Could not find file-type icon for %s', extension)
         icon_path = pathjoin(str(bestdim), 'default.png')
-        if not isfile(pathjon(ICON_ROOT, icon_path)):
+        if not isfile(pathjoin(ICON_ROOT, icon_path)):
             LOGGER.debug('Could not find default file-type icon.')
             return
 
